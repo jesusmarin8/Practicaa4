33a9ba7bba9b4eb410d8ff149f0691dc5afb5802
JMarin <marinjesusalfonso@gmail.com>
2019-12-05 10:05:05 +0100

    Otro Hola Mundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,3 @@
-public class HolaMundo{ aaapublic static void main(String[] args){ System.out.println("Hola mundo"); } }
+public class HolaMundo{ aaapublic static void main(String[] args){ System.out.println("Otro Hola mundo"); } }
 
 
